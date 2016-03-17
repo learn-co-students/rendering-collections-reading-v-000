f1669ff9ffa96de859d12f4476b9e6a235a33c19
Alvinluipo <lualvin1234@gmail.com>
2016-03-17 19:04:36 -0400

    Done.

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,3 +1,4 @@
+=begin
 # == Schema Information
 #
 # Table name: authors
@@ -14,3 +15,4 @@ require 'rails_helper'
 RSpec.describe Author, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+=end
