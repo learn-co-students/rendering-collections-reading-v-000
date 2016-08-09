8de6251b4080e1b98d45aacc7782e71918a0a735
jbok4 <jbok4@aol.com>
2016-08-09 18:50:53 -0400

    complete

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,21 @@
 class AuthorsController < ApplicationController
+
+  def new
+    @author = Author.new
+  end
+
+
   def show
     @author = Author.find(params[:id])
+  end
+
+  private
+  def set_author
+    @author = Author.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def author_params
+    params.require(:author).permit(:name, :hometown)
   end
 end
